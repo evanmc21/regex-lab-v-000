f93a88e6605aae4da07671b53773f5c7eaeeb761
Evan Candler <emcandler21@gmail.com>
2018-02-22 00:02:53 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  !!phone.scan(/\d/).length == 10
+  if phone.scan(/\d/).length == 10
+  else
+    false
 end
